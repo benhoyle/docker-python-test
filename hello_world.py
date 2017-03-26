db56d5a4f5579cb2baf4234ed1702e5d186f3ede
Ben Hoyle <benjhoyle@gmail.com>
2017-03-26 11:09:04 +0100

    Simplify create_app()

--- hello_world.py
+++ hello_world.py
@@ -2,14 +2,13 @@ from flask import Flask
 
 from blueprints import pages
 
-def create_app(settings_override=None):
+def create_app():
     """
     Create a Flask application using the app factory pattern.
-
-    :param settings_override: Override settings
+    
     :return: Flask app
     """
-    app = Flask(__name__, instance_relative_config=True)
+    app = Flask(__name__)
     app.register_blueprint(pages)
 
     return app
